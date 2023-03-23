64f79c9b1cde70fa6a128cf1b0f1f18847ddef81
PavelKrivorotov <pavel.krivorotov.54@mail.ru>
2023-03-23 11:14:47 +0300

    Change task...

--- Labs_3_lisp/lab3.lisp
+++ Labs_3_lisp/lab3.lisp
@@ -1,5 +1,4 @@
 (defvar lst1 nil)
-(defvar lst2 nil)
 
 (set `lst1 `(
         (nil    "1"     "2"     "3"     "4"     "5"     "6"     "7" )
@@ -13,37 +12,93 @@
     )
 )
 
-(set `lst2 `(
-        (nil    "1"     "2"     "3"     "4"     "5"     "6"     "7" )
-        ("1"    nil     5       nil     nil     nil     nil     20  )
-        ("2"    5       nil     20      nil     nil     nil     10  )
-        ("3"    nil     20      nil     15      nil     nil     nil )
-        ("4"    nil     nil     15      nil     5       nil     7   )
-        ("5"    nil     nil     nil     5       nil     40      35  )
-        ("6"    nil     nil     nil     nil     40      nil     12  )
-        ("7"    20      10      nil     7       35      12      nil )
+
+; Stack functional
+(
+    defun getReverse (new old)
+    "Return reverse list..."
+    (cond
+        ((not old) (return-from getReverse new))
+        (T (getReverse (CONS (CAR old) new) (CDR old)))
     )
 )
 
+(
+    defun addToStack (stack item)
+    "Add item to stack (end-position). Return stack."
+    (cond
+        (
+            (not item)
+            stack
+        )
+        
+        (
+            T
+            (getReverse
+                nil
+                (CONS
+                    item
+                    (getReverse nil stack)
+                )
+            )
+        )
+    )
+)
 
 (
-    defun createItem (row-label column-label value)
-    "Create list: (row-label column-label value)"
+    defun getFromStack (stack)
+    "Return item at (end-position)."
+    (CAR
+        (getReverse nil stack)
+    )
+)
+
+(
+    defun delFromStack (stack)
+    "Return stack without element at (end-position)"
+    (getReverse
+        nil
+        (CDR
+            (getReverse nil stack)
+        )
+    )
+)
+
+(
+    defun createStackItem (row-label row row-header-label tmp-lst)
+    "Create stack item."
     (cond
-        (value
+        (
+            (not (or row row-header-label))
+            nil
+        )
+        
+        (
+            T
             (CONS
                 row-label
                 (CONS
-                    column-label
+                    row
                     (CONS
-                        value
-                        nil
+                        row-header-label
+                        (CONS
+                            tmp-lst
+                            nil
+                        )
                     )
                 )
             )
         )
+    )
+)
 
-        (T nil)
+(
+    defun getInStckItem (stack-item index ind)
+    "Return value at position at index in stack-item"
+    (cond
+        ((> ind index) nil)
+        ((equal ind index) (CAR stack-item))
+        (T (getInStckItem (CDR stack-item) index (+ ind 1)))
     )
 )
 
@@ -60,130 +115,411 @@
 )
 
 (
-    defun travelByColumns (new-lst row-label without-label-row header-lst)
-    "Return new-lst..."
-    ; (print row-label)
-    ; (print without-label-row)
-    ; (print (CAR header-lst))
-    ; (terpri)
+    defun existsInLst (lst station)
+    "Return true if value in lst, else nil"
+    (cond
+        ((not lst) nil)
+        ((equal (CAR lst) station) T)
+        (T (existsInLst (CDR lst) station))
+    )
+)
+
+(
+    defun addToTmpLst (lst station value)
+    "Return list with new station and change value of travel"
+    (cond
+        (
+            (not lst)
+            (CONS
+                (CONS station nil)
+                (CONS value nil)
+            )
+        )
+
+        (
+            T
+            (CONS
+                (getReverse
+                    nil
+                    (CONS
+                        station
+                        (getReverse nil (CAR lst))
+                    )
+                )
+                (CONS
+                    (+
+                        value
+                        (CAR
+                            (CDR lst)
+                        )
+                    )
+                    nil
+                )
+            )
+        )
+    )
+)
+
+(
+    defun getRowByLabel (lst-withoter-header-label station)
+    "Get list where 1st element = station. Else nil"
+    (cond
+        ((not lst-withoter-header-label) nil)
+
+        (
+            (equal
+                (CAR
+                    (CAR lst-withoter-header-label)
+                )
+                station
+            )
+            (CAR lst-withoter-header-label)
+        )
+
+        (
+            T
+            (getRowByLabel
+                (CDR lst-withoter-header-label)
+                station
+            )
+        )
+    )
+)
+
+(
+    defun iterFromLst (new-lst lst stack station-to)
+    "Return all travels between station-from station-to."
+    (print "new-lst:")
+    (print new-lst)
+    (terpri)
+
+    (print "lst:")
+    (print lst)
+    (terpri)
+
+    (print "stack:")
+    (print stack)
+    (terpri)
+
+    (print "station-to:")
+    (print station-to)
+    (terpri)
+    (terpri)
+    (terpri)
 
     (cond
-        (without-label-row
-            (travelByColumns
+        ; Stack is nil
+        ((not stack) new-lst)
+        
+        ; Value is nil
+        (
+            (not
+                (CAR
+                    (getInStckItem
+                        (getFromStack stack)
+                        1
+                        0
+                    )
+                )
+            )
+            
+            (iterFromLst
+                new-lst
+                lst
+                (addToStack
+                    (delFromStack stack)
+                    (createStackItem
+                        (getInStckItem
+                            (getFromStack stack)
+                            0
+                            0
+                        )
+                        (CDR
+                            (getInStckItem
+                                (getFromStack stack)
+                                1
+                                0
+                            )
+                        )
+                        (CDR
+                            (getInStckItem
+                                (getFromStack stack)
+                                2
+                                0
+                            )
+                        )
+                        (getInStckItem
+                            (getFromStack stack)
+                            3
+                            0
+                        )
+                    )
+                )
+                station-to
+            )
+        )
+        
+        ; Value not nil and is to-station
+        (
+            (and
+                (CAR
+                    (getInStckItem
+                        (getFromStack stack)
+                        1
+                        0
+                    )
+                )
+                (equal
+                    (CAR
+                        (getInStckItem
+                            (getFromStack stack)
+                            2
+                            0
+                        )
+                    )
+                    station-to
+                )
+            )
+
+            (iterFromLst
                 (addToLst
                     new-lst
-                    (createItem
-                        row-label
-                        (CAR header-lst)
-                        (CAR without-label-row)
+                    (addToTmpLst
+                        (getInStckItem
+                            (getFromStack stack)
+                            3
+                            0
+                        )
+                        station-to
+                        (CAR
+                            (getInStckItem
+                                (getFromStack stack)
+                                1
+                                0
+                            )
+                        )
                     )
                 )
 
-                row-label
-                (CDR without-label-row)
-                (CDR header-lst)
+                lst
+                (addToStack
+                    (delFromStack stack)
+                    (createStackItem
+                        (getInStckItem
+                            (getFromStack stack)
+                            0
+                            0
+                        )
+                        
+                        (CDR
+                            (getInStckItem
+                                (getFromStack stack)
+                                1
+                                0
+                            )
+                        )
+                        (CDR
+                            (getInStckItem
+                                (getFromStack stack)
+                                2
+                                0
+                            )
+                        )
+                        (getInStckItem
+                            (getFromStack stack)
+                            3
+                            0
+                        )
+                    )
+                )
+                station-to
             )
         )
 
-        (T new-lst)
-    )
-)
-
-(
-    defun travelByRows (new-lst without-header-lst header-lst)
-    "Return new-lst. Travel by all rows in without-header-lst"
-    ; (print new-lst)
-    ; (print without-header-lst)
-    ; (prin1 header-lst)
-    ; (terpri)
-
-    (cond
-        (without-header-lst
-            (travelByRows
-                (travelByColumns
-                    new-lst
+        ; Value exists in tmp-lst. Brake cicle.
+        (
+            (and
+                (CAR
+                    (getInStckItem
+                        (getFromStack stack)
+                        1
+                        0
+                    )
+                )
+                (existsInLst
                     (CAR
-                        (CAR without-header-lst)
+                        (getInStckItem
+                            (getFromStack stack)
+                            3
+                            0
+                        )
                     )
-
-                    (CDR
-                        (CAR without-header-lst)
+                    (CAR
+                        (getInStckItem
+                            (getFromStack stack)
+                            2
+                            0
+                        )
                     )
-
-                    (CDR header-lst)
                 )
-                (CDR without-header-lst)
-                header-lst
+            )
+
+            (iterFromLst
+                new-lst
+                lst
+                (addToStack
+                    (delFromStack stack)
+                    (createStackItem
+                        (getInStckItem
+                            (getFromStack stack)
+                            0
+                            0
+                        )
+                        (CDR
+                            (getInStckItem
+                                (getFromStack stack)
+                                1
+                                0
+                            )
+                        )
+                        (CDR
+                            (getInStckItem
+                                (getFromStack stack)
+                                2
+                                0
+                            )
+                        )
+                        (getInStckItem
+                            (getFromStack stack)
+                            3
+                            0
+                        )
+                    )
+                )
+                station-to
             )
         )
 
-        (T new-lst)
-    )
-)
+        ; Value not nil and is not to-station
+        (
+            T
 
-(
-    defun getValueInItem (item)
-    "Return the Value in item"
-    (CAR
-        (CDR
-            (CDR item)
+            (iterFromLst
+                new-lst
+                lst
+                
+                (addToStack
+                    (addToStack
+                        (delFromStack stack)
+                        (createStackItem
+                            (getInStckItem
+                                (getFromStack stack)
+                                0
+                                0
+                            )
+                            (CDR
+                                (getInStckItem
+                                    (getFromStack stack)
+                                    1
+                                    0
+                                )
+                            )
+                            (CDR
+                                (getInStckItem
+                                    (getFromStack stack)
+                                    2
+                                    0
+                                )
+                            )
+                            (getInStckItem
+                                (getFromStack stack)
+                                3
+                                0
+                            )
+                        )
+                    )
+                    
+                    
+                    (createStackItem
+                        (CAR
+                            (getInStckItem
+                                (getFromStack stack)
+                                2
+                                0
+                            )
+                        )
+                        (CDR
+                            (getRowByLabel
+                                (CDR lst)
+                                (CAR
+                                    (getInStckItem
+                                        (getFromStack stack)
+                                        2
+                                        0
+                                    )
+                                )
+                            )
+                        )
+                        (CDR
+                            (CAR lst)
+                        )
+                        (addToTmpLst
+                            (getInStckItem
+                                (getFromStack stack)
+                                3
+                                0
+                            )
+                            (CAR
+                                (getInStckItem
+                                    (getFromStack stack)
+                                    2
+                                    0
+                                )
+                            )
+                            (CAR
+                                (getInStckItem
+                                    (getFromStack stack)
+                                    1
+                                    0
+                                )
+                            )
+                        )
+                    )
+                )
+                
+                station-to
+            )
         )
     )
 )
 
 (
-    defun getMinimalItems (new-lst lst)
-    "Return the minimal items in lst"
-    (cond
-        ((not lst) new-lst)
-        ((not new-lst) (getMinimalItems (addToLst new-lst (CAR lst)) (CDR lst)))
-        ((< (getValueInItem (CAR lst)) (getValueInItem (CAR new-lst)))
-            (getMinimalItems (addToLst nil (CAR lst)) (CDR lst))
+    defun getTravels (lst station-from station-to)
+    "Return the list of all travels between station-from to station-to"
+    (iterFromLSt
+        nil
+        lst
+        (addToStack
+            nil
+            (createStackItem
+                (CAR
+                    (getRowByLabel (CDR lst) station-from)
+                )
+                (CDR
+                    (getRowByLabel (CDR lst) station-from)
+                )
+                (CDR
+                    (CAR lst)
+                )
+                (addToTmpLst nil station-from 0)
+            )
         )
-
-        ((= (getValueInItem (CAR lst)) (getValueInItem (CAR new-lst)))
-            (getMinimalItems (addToLst new-lst (CAR lst)) (CDR lst))
-        )
-
-        (T (getMinimalItems new-lst (CDR lst)))
+        station-to
     )
 )
 
-(
-    defun getTravels (new-lst lst)
-    "Return new-list of minimal travels"
-    (travelByRows nil (CDR lst) (CAR lst))
+
+
+(print
+    (getTravels lst1 "5" "3")
 )
-
-(
-    defun getMinimalTravels (lst)
-    "Return the minimal travels in lst"
-    (getMinimalItems nil (getTravels nil lst))
-)
-
-
-(print "List of the distance  1:")
-(print lst1)
-(terpri)
-
-(print "List of all travels between station:")
-(print (getTravels nil lst1) )
-(terpri)
-
-(print "List of minimal travels in all travels:")
-(print (getMinimalTravels lst1))
-(terpri)
-
-
-(terpri)
-(print "List of the distance  2:")
-(print lst1)
-(terpri)
-
-(print "List of all travels between station:")
-(print (getTravels nil lst2) )
-(terpri)
-
-(print "List of minimal travels in all travels:")
-(print (getMinimalTravels lst2))
-(terpri)
