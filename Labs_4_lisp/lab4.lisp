e244ead26a855374973a7fc33850a33a58b07e18
PavelKrivorotov <pavel.krivorotov.54@mail.ru>
2023-03-25 18:34:51 +0300

    Add tickets functional and queryes.

--- Labs_4_lisp/lab4.lisp
+++ Labs_4_lisp/lab4.lisp
@@ -1,5 +1,6 @@
 (defvar railway-table nil)
-(defvar railway-struct-label "railway-struct-")
+(defvar ticket-table nil)
+
 
 (defstruct railway-struct
     flight-number
@@ -8,6 +9,11 @@
     station-to
     date-from
     date-to
+)
+
+(setf
+    ticket-table
+    (make-sequence `LIST 15)
 )
 
 
@@ -184,6 +190,10 @@
                 flight-number-1
                 (railway-struct-flight-number (CAR table))
             )
+            
+            ; Add functional
+            (delTicketMultiple ticket-table flight-number-1)
+            
             (delFromTable
                 new-table
                 (CDR table)
@@ -203,7 +213,99 @@
 )
 
 
-; Declare functions for stations
+; Declare functions for tickets:
+(
+    defun addTicket (ticket-table flight-number-1)
+    "Add ticket to ticket-table."
+    (cond
+        (
+            (not (CDR ticket-table))
+            nil
+        )
+        
+        (
+            (not (CAR ticket-table))
+            (setf
+                (CAR ticket-table)
+                (list
+                    flight-number-1
+                    (random 1000000000)
+                )
+            )
+        )
+        
+        (
+            T
+            (addTicket (CDR ticket-table) flight-number-1)
+        )
+    )
+)
+
+(
+    defun delTicket (ticket-table ticket-number-1)
+    "Delete ticket by ticket-number."
+    (cond
+        (
+            (not (CDR ticket-table))
+            nil
+        )
+        
+        (
+            (equal
+                ticket-number-1
+                (CAR
+                    (CDR
+                        (CAR ticket-table)
+                    )
+                )
+            )
+            (setf
+                (CAR ticket-table)
+                nil
+            )
+        )
+        
+        (
+            T
+            (delTicket (CDR ticket-table) ticket-number-1)
+        )
+    )
+)
+
+(
+    defun delTicketMultiple (ticket-table flight-number-1)
+    "Delete ticket by flight-number."
+    (cond
+        (
+            (not (CDR ticket-table))
+            nil
+        )
+        
+        (
+            (equal
+                flight-number-1
+                (CAR
+                    (CAR ticket-table)
+                )
+            )
+            
+            (setf
+                (CAR ticket-table)
+                nil
+            )
+            (delTicketMultiple (CDR ticket-table) flight-number-1)
+        )
+        
+        (
+            T
+            (delTicketMultiple (CDR ticket-table) flight-number-1)
+        )
+    )
+)
+
+
+;Declare functions for search:
+
 
 
 
@@ -238,31 +340,31 @@
     "13.01.2023"
 )
 
-
-; (delFromTable
-;     nil
-;     railway-table
-;     1
-; )
-
-
-; (print
-;     (getFromTable railway-table 3)
-; )
-
+(addToTable
+    railway-table
+    4
+    ""
+    ""
+    ""
+    ""
+    ""
+)
 
 
 (print
-    railway-table
+    "Add to railway-table new railways:"
 )
 (terpri)
-(terpri)
+(print
+    railway-table
+)
 
 
 
+; Update from railway-table:
 (setToTable
     railway-table
-    1
+    4
     `(
         "train-number"
         "station-from"
@@ -271,15 +373,297 @@
         "date-to"
     )
     `(
-        "KOKOKOK"
-        "NEW-STATION"
-        "OKEY"
-        "00.00.0000"
-        "99.99.9999"
+        "QQQ-200"
+        "Saint-Petersburg"
+        "Pscov"
+        "14.01.2023"
+        "14.01.2023"
+    )
+)
+
+(terpri)
+(terpri)
+(terpri)
+(print
+    "Update from railway, where flight-number = 4"
+)
+(terpri)
+(print
+    railway-table
+)
+
+
+; Add tickets to ticket-table:
+(addTicket ticket-table 1)
+(addTicket ticket-table 1)
+(addTicket ticket-table 1)
+(addTicket ticket-table 2)
+(addTicket ticket-table 2)
+(addTicket ticket-table 3)
+(addTicket ticket-table 4)
+(addTicket ticket-table 4)
+
+
+(terpri)
+(terpri)
+(terpri)
+(print
+    "Add tickets:"
+)
+(terpri)
+(print
+    ticket-table
+)
+
+
+
+; Delete ticket by ticket-number:
+(delTicket ticket-table 277986140)
+
+
+(terpri)
+(terpri)
+(terpri)
+(print
+    "Delete ticket by ticket-number, where ticket-number = 277986140:"
+)
+(terpri)
+(print
+    ticket-table
+)
+
+
+
+; Delete railway, where flight-number = 1:
+(delFromTable nil railway-table 1)
+
+
+(terpri)
+(terpri)
+(terpri)
+(print
+    "Delete railway, where flight-number = 1:"
+)
+(terpri)
+(print
+    "Now railway-table:"
+)
+(terpri)
+(print
+    railway-table
+)
+(terpri)
+(print
+    "Now ticket-table:"
+)
+(terpri)
+(print
+    ticket-table
+)
+
+
+
+; Search by railway-table and ticket-table:
+(
+    defun search-railways-by-station-from (lst station-from)
+    "..."
+    (mapcan
+        (function
+            (lambda
+                (railway)
+                (cond
+                    (
+                        (equal
+                            station-from
+                            (railway-struct-station-from railway)
+                        )
+                        (list railway)
+                    )
+                    
+                    (T nil)
+                )
+            )
+        )
+        lst
+    )
+)
+
+(
+    defun search-railways-by-station-to (lst station-to)
+    "..."
+    (mapcan
+        (function
+            (lambda
+                (railway)
+                (cond
+                    (
+                        (equal
+                            station-to
+                            (railway-struct-station-to railway)
+                        )
+                        (list railway)
+                    )
+                    
+                    (T nil)
+                )
+            )
+        )
+        lst
+    )
+)
+
+(
+    defun search-railways-by-stations (lst station-from station-to)
+    "..."
+    (mapcan
+        (function
+            (lambda
+                (railway)
+                (cond
+                    (
+                        (and
+                            (equal
+                                station-from
+                                (railway-struct-station-from railway)
+                            )
+                            (equal
+                                station-to
+                                (railway-struct-station-to railway)
+                            )
+                        )
+                        (list railway)
+                    )
+                    
+                    (T nil)
+                )
+            )
+        )
+        lst
     )
 )
 
 
+
+(
+    defun search-tickets-by-flight-number (lst flight-number)
+    "..."
+    (mapcan
+        (function
+            (lambda
+                (lst)
+                (cond
+                    (
+                        (equal
+                            flight-number
+                            (CAR lst)
+                        )
+                        (CDR lst)
+                    )
+                    
+                    (T nil)
+                )
+            )
+        )
+        ; ticket-table
+        lst
+    )
+)
+
+(
+    defun search-tickets-by-date-from (lst1 lst2 date-from)
+    "..."
+    (mapcan
+        (function
+            (lambda
+                (lst)
+                (cond
+                    (
+                        (equal lst nil)
+                        nil
+                    )
+                    
+                    (
+                        (equal
+                            date-from
+                            (railway-struct-date-from
+                                (getFromTable
+                                    lst1
+                                    (CAR lst)
+                                )
+                            )
+                        )
+                        (CDR lst)
+                    )
+                    
+                    (T nil)
+                )
+            )
+        )
+        lst2
+    )
+)
+
+
+
+(terpri)
+(terpri)
+(terpri)
 (print
-    railway-table
+    "Serach from railway-table:"
+)
+(terpri)
+
+(print
+    "Where station-from = Saint-Petersburg:"
+)
+(terpri)
+
+(print
+    (search-railways-by-station-from railway-table "Saint-Petersburg")
+)
+(terpri)
+
+(print
+    "Where station-to = Ivanovo:"
+)
+(terpri)
+
+(print
+    (search-railways-by-station-to railway-table "Ivanovo")
+)
+(terpri)
+
+(print
+    "Where station-from = Saint-Petersburg, station-to = Pscov:"
+)
+(terpri)
+
+(print
+
+    (search-railways-by-stations railway-table "Saint-Petersburg" "Pscov" )
+)
+
+
+
+(terpri)
+(terpri)
+(terpri)
+(print
+    "Search tickets, where flight-number = 4:"
+)
+
+(terpri)
+(print
+    (search-tickets-by-flight-number ticket-table 4)
+)
+
+(terpri)
+
+(print
+    "Search tickets where date-from = 11.01.2023:"
+)
+(terpri)
+
+(print
+    (search-tickets-by-date-from railway-table ticket-table "11.01.2023")
 )
